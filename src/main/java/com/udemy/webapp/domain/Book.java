2fedeca384566b75227d0285d51bef156a557d0f
ChonkaWork <chonka.work@gmail.com>
2021-04-19 12:22:58 +0300

    - Added controllers and templates - connected with h2 db

--- src/main/java/com/udemy/webapp/domain/Book.java
+++ src/main/java/com/udemy/webapp/domain/Book.java
@@ -95,17 +95,11 @@ public class Book {
 
     @Override
     public String toString() {
-        return "Book{"
-                + "id="
-                + id
-                + ", title='"
-                + title
-                + '\''
-                + ", isbn='"
-                + isbn
-                + '\''
-                + ", authors="
-                + authors
-                + '}';
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", publisher=" + publisher +
+                '}';
     }
 }
